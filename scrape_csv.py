4950c205630ef2719804e0ba617d1c1db9e4066f
TheoRhytical <rhyscabonita@gmail.com>
2024-02-10 04:51:06 +0800

    Parse robots.txt

--- scrape_csv.py
+++ scrape_csv.py
@@ -1,7 +1,19 @@
 # Web scraping script that downloads csv files in given static website
 # TODO: (and optionally xlsx) add option to download xlsx files
+import requests
 import argparse
+import urllib.robotparser as robotparser
 
+# Note sure if I wanna run this as a standalone script or allow to be imported as a module
+# class Scraper:
+#     def __init__(self, website: str, output_dir: str):
+#         self.website = website
+#         self.output_dir = output_dir
+
+# if __name__ == "__main__":
+
+
+# ======== Arg Parsing ======== #
 parser = argparse.ArgumentParser(
     prog='CSV Web Scraper',
     description="Scrapes a website's csv files"
@@ -12,5 +24,22 @@ parser.add_argument('-o', '--output', help='output folder')
 args = parser.parse_args()
 
 # Access the values of the flags
-website = args.website
-output_folder = args.output
+website: str = args.website
+output_folder: str = args.output
+
+# ======== Actual Crawling ======== #
+
+if not website.endswith("/"):
+    website += "/"
+robots = requests.get(website + "robots.txt")
+
+# Check if there is robots.txt
+if robots.status_code != 200:
+    print("Could not read "+website+"robots.txt")
+    exit
+
+# Parse robots.txt
+rp = robotparser.RobotFileParser()
+rp.set_url(website + "robots.txt")
+rp.read()
+sitemap = rp.site_maps()
